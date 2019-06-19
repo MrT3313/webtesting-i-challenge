093f575c28035e94555d90c4bccd0eb38e63fac4
MrT3313 <rturgeon13@gmail.com>
2019-06-19 14:55:29 -0500

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,48 @@
 module.exports = {
+  repair,
+
   succeed,
   fail,
-  repair,
   get,
   add,
 };
 
+function repair(item) {
+  if (Object.entries(item).length === 0 ) {
+     return item
+  } else {
+    // -- V2
+    item.durability = 100
+  
+    return {
+      ...item,
+      // -- V1
+      // durability: 100
+    }
+  }
+}
+
 function succeed(item) {
+  if(item.enhancement < 20) {
+    item.enhancement += 1
+  }  
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -= 5
+  } else {
+    item.durability -= 10
+  }
+  if (item.enhancement > 16 ) {
+    item.enhancement -= 1
+  } 
+
+
   return { ...item };
 }
 
-function repair(item) {
-  return { ...item };
-}
 
 function get(item) {
   return { ...item };
